493f01d4ecfb73f9ae4688f434817eb68424add1
flo071 <flo.hintermeier@gmail.com>
2017-02-21 14:53:36 +0100

    Update Graph

--- graph.php
+++ graph.php
@@ -8,27 +8,27 @@
     <script language="JavaScript">
         function displayLineChart() {
             var data = {
-                labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+                labels: [1, 2, 3, 4, 5, 6, 7],
                 datasets: [{
-                        label: "Prime and Fibonacci",
-                        fillColor: "rgba(220,220,220,0.2)",
-                        strokeColor: "rgba(220,220,220,1)",
-                        pointColor: "rgba(220,220,220,1)",
+                        label: "Inside",
+                        fillColor: "rgba(124,220,220,0.2)",
+                        strokeColor: "rgba(124,220,220,1)",
+                        pointColor: "rgba(124,220,220,1)",
                         pointStrokeColor: "#fff",
                         pointHighlightFill: "#fff",
-                        pointHighlightStroke: "rgba(220,220,220,1)",
-                        data: [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+                        pointHighlightStroke: "rgba(124,220,220,1)",
+                        data: [17, 23, 75, 32, 65, 5, 100]
                     },
                     {
-                        label: "My Second dataset",
-                        fillColor: "rgba(151,187,205,0.2)",
-                        strokeColor: "rgba(151,187,205,1)",
-                        pointColor: "rgba(151,187,205,1)",
-                        pointStrokeColor: "#fff",
-                        pointHighlightFill: "#fff",
-                        pointHighlightStroke: "rgba(151,187,205,1)",
-                        data: [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
-                    }
+                          label: "Outside",
+                          fillColor: "rgba(124,220,220,0.2)",
+                          strokeColor: "rgba(124,124,220,1)",
+                          pointColor: "rgba(124,124,220,1)",
+                          pointStrokeColor: "#fff",
+                          pointHighlightFill: "#fff",
+                          pointHighlightStroke: "rgba(124,124,220,1)",
+                          data: [100, 5, 65, 32, 75, 23, 17]
+                      }
                 ]
             };
             var ctx = document.getElementById("lineChart").getContext("2d");
@@ -41,7 +41,6 @@
 <body onload="displayLineChart();">
   <center>
     <div class="box">
-        <canvas id="lineChart" height="450" width="800"></canvas>
-    </div>
+        <canvas id="lineChart" height="450" width="800"></canvas></div>
   </center>
 </body>
