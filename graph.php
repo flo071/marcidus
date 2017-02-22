f803d643b4f00e6643ada19750cf3132552dcce2
flo071 <flo.hintermeier@gmail.com>
2017-02-22 11:27:49 +0100

    add a legend

--- graph.php
+++ graph.php
@@ -20,15 +20,15 @@
                         data: [17, 23, 75, 32, 65, 5, 100]
                     },
                     {
-                          label: "Outside",
-                          fillColor: "rgba(124,220,220,0.2)",
-                          strokeColor: "rgba(124,124,220,1)",
-                          pointColor: "rgba(124,124,220,1)",
-                          pointStrokeColor: "#fff",
-                          pointHighlightFill: "#fff",
-                          pointHighlightStroke: "rgba(124,124,220,1)",
-                          data: [100, 5, 65, 32, 75, 23, 17]
-                      }
+                        label: "Outside",
+                        fillColor: "rgba(124,220,220,0.2)",
+                        strokeColor: "rgba(124,124,220,1)",
+                        pointColor: "rgba(124,124,220,1)",
+                        pointStrokeColor: "#fff",
+                        pointHighlightFill: "#fff",
+                        pointHighlightStroke: "rgba(124,124,220,1)",
+                        data: [100, 5, 65, 32, 75, 23, 17]
+                    }
                 ]
             };
             var ctx = document.getElementById("lineChart").getContext("2d");
@@ -39,11 +39,60 @@
 </head>
 
 <body onload="displayLineChart();">
-  <center>
-    <div class="box">
-        <canvas id="lineChart" height="450" width="800"></canvas></div>
-    <div class="legend">
-        <p style="color: #7ddddd">Inside</p>
-        <p style="color: #7d7ddd">Outside</p></div>
-  </center>
+    <center>
+        <div class="box">
+            <canvas id="lineChart" height="450" width="800"></canvas></div>
+        <div class="legend">
+            <p style="color: #7ddddd">Inside</p>
+            <p style="color: #7d7ddd">Outside</p>
+        </div>
+        <div class="list">
+            <table summary="Luftfeuchtigkeit" cellpadding="1" cellspacing="10">
+                <thead>
+                    <tr>
+                        <th class="d">Day</th>
+                        <th class="i">Inside</th>
+                        <th class="o">Outside</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td class="d">1</td>
+                        <td class="i">17</td>
+                        <td class="o">100</td>
+                    </tr>
+                    <tr>
+                        <td class="d">2</td>
+                        <td class="i">23</td>
+                        <td class="o">5</td>
+                    </tr>
+                    <tr>
+                        <td class="d">3</td>
+                        <td class="i">75</td>
+                        <td class="o">65</td>
+                    </tr>
+                    <tr>
+                        <td class="d">4</td>
+                        <td class="i">32</td>
+                        <td class="o">32</td>
+                    </tr>
+                    <tr>
+                        <td class="d">5</td>
+                        <td class="i">65</td>
+                        <td class="o">75</td>
+                    </tr>
+                    <tr>
+                        <td class="d">6</td>
+                        <td class="i">5</td>
+                        <td class="o">23</td>
+                    </tr>
+                    <tr>
+                        <td class="d">7</td>
+                        <td class="i">100</td>
+                        <td class="o">17</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </center>
 </body>
