abefb23bc931ad2a6b4487c754d1e75ac4966f14
flo071 <flo.hintermeier@gmail.com>
2017-02-28 18:32:53 +0100

    more material design

--- graph.php
+++ graph.php
@@ -44,49 +44,77 @@
 </head>
 
 <body onload="displayLineChart();">
-    <a href="index.html"><button class="mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect" > <i class="material-icons">arrow_back</i> </button></a>
-    <center>
-        <div class="box">
-            <canvas id="lineChart" height="450" width="800"></canvas></div>
-        <div class="legend">
-            <p style="color: #7ddddd">Inside</p>
-            <p style="color: #7d7ddd">Outside</p>
-        </div>
-        <div class="list">
-            <table border="1">
-                <tr>
-                    <td>Time</td>
-                    <td>Location</td>
-                    <td>Sensor ID</td>
-                    <td>Inside</td>
-                    <td>Outside</td>
-                    <td>Unit</td>
-                </tr>
-                <?php
-              $link= mysqli_connect("localhost","root","mysql","Luftfeuchtigkeit");
-              mysqli_set_charset($link,"utf8");
-              $sql = "SELECT sta_name, sen_id, mk_einheit, md_messwert_i, md_messwert_o, md_timestamp
-                      FROM tbl_standort, tbl_messkat, tbl_sensoren, tbl_messdaten
-                      WHERE md_sen_id_fk = sen_id
-                      AND sta_id = sen_sta_id_fk
-                      AND mk_id = md_mk_id_fk";
-              $result = mysqli_query($link,$sql);
+  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-drawer
+          mdl-layout--fixed-header">
+      <header class="mdl-layout__header">
+          <div class="mdl-layout__header-row">
+              <div class="mdl-layout-spacer"></div>
+              <div class="mdl-textfield mdl-js-textfield mdl-textfield--expandable
+                mdl-textfield--floating-label mdl-textfield--align-right">
+                  <label class="mdl-button mdl-js-button mdl-button--icon" for="fixed-header-drawer-exp">
+        <i class="material-icons">search</i>
+      </label>
+                  <div class="mdl-textfield__expandable-holder">
+                      <input class="mdl-textfield__input" type="text" name="sample" id="fixed-header-drawer-exp">
+                  </div>
+              </div>
+          </div>
+      </header>
+      <div class="mdl-layout__drawer">
+          <span class="mdl-layout-title">Marcidus</span>
+          <nav class="mdl-navigation">
+              <a class="mdl-navigation__link" href="hardware.html">Hardware & Software</a>
+              <a class="mdl-navigation__link" href="graph.php">Graph</a>
+              <a class="mdl-navigation__link" href="create.html">Creators</a>
+          </nav>
+      </div>
+      <main class="mdl-layout__content">
+          <div class="page-content">
+              <center>
+                <div class="box">
+                    <canvas id="lineChart" height="450" width="800"></canvas></div>
+                <div class="legend">
+                    <p style="color: #7ddddd">Inside</p>
+                    <p style="color: #7d7ddd">Outside</p>
+                </div>
+                <div class="list">
+                    <table border="1">
+                        <tr>
+                            <td>Time</td>
+                            <td>Location</td>
+                            <td>Sensor ID</td>
+                            <td>Inside</td>
+                            <td>Outside</td>
+                            <td>Unit</td>
+                        </tr>
+                        <?php
+                      $link= mysqli_connect("localhost","root","mysql","Luftfeuchtigkeit");
+                      mysqli_set_charset($link,"utf8");
+                      $sql = "SELECT sta_name, sen_id, mk_einheit, md_messwert_i, md_messwert_o, md_timestamp
+                              FROM tbl_standort, tbl_messkat, tbl_sensoren, tbl_messdaten
+                              WHERE md_sen_id_fk = sen_id
+                              AND sta_id = sen_sta_id_fk
+                              AND mk_id = md_mk_id_fk";
+                      $result = mysqli_query($link,$sql);
 
-              while($row=mysqli_fetch_array($result))
-          		{
-                echo "<tr>";
-                echo "<td>".$row["md_timestamp"]."</td>";
-                echo "<td>".$row["sta_name"]."</td>";
-                echo "<td>".$row["sen_id"]."</td>";
-                  echo "<td>".$row["md_messwert_i"]."</td>";
-                //echo "<br>";
-                echo "<td>".$row["md_messwert_o"]."</td>";
-                echo "<td>".$row["mk_einheit"]."</td>";
-                //echo "<br>";
-                echo "</tr>";
-          		}
-            ?>
-            </table>
-        </div>
-    </center>
+                      while($row=mysqli_fetch_array($result))
+                  		{
+                        echo "<tr>";
+                        echo "<td>".$row["md_timestamp"]."</td>";
+                        echo "<td>".$row["sta_name"]."</td>";
+                        echo "<td>".$row["sen_id"]."</td>";
+                          echo "<td>".$row["md_messwert_i"]."</td>";
+                        //echo "<br>";
+                        echo "<td>".$row["md_messwert_o"]."</td>";
+                        echo "<td>".$row["mk_einheit"]."</td>";
+                        //echo "<br>";
+                        echo "</tr>";
+                  		}
+                    ?>
+                    </table>
+                </div>
+              </center>
+          </div>
+      </main>
+  </div>
 </body>
